b0bf47fa1a9c18ead6adf2b55a3031626f49dbdf
Russkazkin <Mas2017ter>
2019-02-11 15:32:48 +0300

    gitignore updated, index.php restored

---
+++ wp-content/themes/basic-theme-with-bootstrap-4/index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="content-wrapper">
+    <div class="container">
+        <div class="main-heading">
+            <h1><?php the_title(); ?></h1>
+        </div>
+        <section class="page-content">
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post();
+                    the_content();
+                endwhile;
+            endif; ?>
+        </section>
+    </div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
