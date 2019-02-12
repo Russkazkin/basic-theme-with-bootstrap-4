d948d60a258e157f9cc8eba51d93c884344ea3c6
Russkazkin <Mas2017ter>
2019-02-12 15:54:37 +0300

    Jumbotron text customizer added

--- wp-content/themes/basic-theme-with-bootstrap-4/customizer/custom_jumbotron.php
+++ wp-content/themes/basic-theme-with-bootstrap-4/customizer/custom_jumbotron.php
@@ -24,5 +24,98 @@ function bbsCustomJumbotron( $wp_customize ){
             'type'      => 'checkbox'
         )
     );
+    $wp_customize->add_setting(
+        'JumbotronHeading',
+        array(
+            'default' => 'Hello, world!',
+            'sanitize_callback' => 'bbsSanitize',
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'JumbotronHeading',
+        array(
+            'section' => 'jumbotronOptions',
+            'label' => 'Jumbotron heading',
+            'type' => 'text'
+        )
+    );
+    $wp_customize->add_setting(
+        'JumbotronBigText',
+        array(
+            'default' => 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.',
+            'sanitize_callback' => 'bbsSanitize',
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'JumbotronBigText',
+        array(
+            'section' => 'jumbotronOptions',
+            'label' => 'Jumbotron Big Text',
+            'type' => 'textarea'
+        )
+    );
+    $wp_customize->add_setting(
+        'hideJumbotronBigText',
+        array(
+            'default' => false,
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'hideJumbotronBigText',
+        array(
+            'section'   => 'jumbotronOptions',
+            'label'     => 'Hide jumbotron\'s big text',
+            'type'      => 'checkbox'
+        )
+    );
+    $wp_customize->add_setting(
+        'hideJumbotronSeparator',
+        array(
+            'default' => false,
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'hideJumbotronSeparator',
+        array(
+            'section'   => 'jumbotronOptions',
+            'label'     => 'Hide jumbotron\'s separator',
+            'type'      => 'checkbox'
+        )
+    );
+    $wp_customize->add_setting(
+        'JumbotronSmallText',
+        array(
+            'default' => 'It uses utility classes for typography and spacing to space content out within the larger container.',
+            'sanitize_callback' => 'bbsSanitize',
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'JumbotronSmallText',
+        array(
+            'section' => 'jumbotronOptions',
+            'label' => 'Jumbotron Small Text',
+            'type' => 'textarea'
+        )
+    );
+    $wp_customize->add_setting(
+        'hideJumbotronSmallText',
+        array(
+            'default' => false,
+            'transport' => 'refresh'
+        )
+    );
+    $wp_customize->add_control(
+        'hideJumbotronSmallText',
+        array(
+            'section'   => 'jumbotronOptions',
+            'label'     => 'Hide jumbotron\'s small text',
+            'type'      => 'checkbox'
+        )
+    );
 }
 add_action( 'customize_register', 'bbsCustomJumbotron' );
--- wp-content/themes/basic-theme-with-bootstrap-4/inc/bbs_jumbotron.php
+++ wp-content/themes/basic-theme-with-bootstrap-4/inc/bbs_jumbotron.php
@@ -3,10 +3,16 @@ if(!function_exists('frontPageJumbotron')){
 function frontPageJumbotron(){ ?>
     <div class="container jumbotron-wrap mt-5">
         <div class="jumbotron">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <h1 class="display-4"><?= get_theme_mod('JumbotronHeading', 'Hello, world!');?></h1>
+            <?php if(!get_theme_mod('hideJumbotronBigText', false)): ?>
+            <p class="lead"><?= get_theme_mod('JumbotronBigText', 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.');?></p>
+            <?php endif; ?>
+            <?php if(!get_theme_mod('hideJumbotronSeparator', false)): ?>
             <div class="bbs-separator"></div>
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <?php endif; ?>
+            <?php if(!get_theme_mod('hideJumbotronSmallText', false)): ?>
+            <p><?= get_theme_mod('JumbotronSmallText', 'It uses utility classes for typography and spacing to space content out within the larger container.');?></p>
+            <?php endif; ?>
             <button type="button" class="btn btn-info btn-lg btn-bss">Order</button>
         </div>
     </div>
