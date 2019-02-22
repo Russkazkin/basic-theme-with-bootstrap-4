d5c8e8af8b93ec1e4589c93a900ce89596cdb198
Russkazkin-home <russkazkin@gmail.com>
2019-02-22 10:19:28 +0300

    libraries update (probably)

--- wp-content/themes/basic-theme-with-bootstrap-4/404.php
+++ wp-content/themes/basic-theme-with-bootstrap-4/404.php
@@ -11,6 +11,6 @@
 
             </article>
 
-    </main> <!-- #content -->
+    </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/basic-theme-with-bootstrap-4/customizer/custom_modal.php
+++ wp-content/themes/basic-theme-with-bootstrap-4/customizer/custom_modal.php
@@ -1,1 +1,7 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: ruslan_mbpr12
+ * Date: 2019-02-13
+ * Time: 12:34
+ */
