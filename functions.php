ad2bcc0f32ee3849772b3f7f02b3c9a8a587a1c1
Russkazkin <ruslan@skazkin.su>
2020-07-15 14:06:38 +0300

    Add yandex metric

--- functions.php
+++ functions.php
@@ -117,3 +117,24 @@ function start_session() {
     }
 }
 add_action('init', 'start_session', 1);
+
+//Метрики
+
+function metrics() { ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(65631721, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/65631721" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+<?php }
+
+add_action('wp_head', 'metrics');
